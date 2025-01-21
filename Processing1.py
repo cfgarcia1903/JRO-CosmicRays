1cc4ce8eac19d40b17b9e4fb00e6aa6875028034
Carlos Francisco García Hipólito <cfgarcia@pucp.edu.pe>
2025-01-21 13:32:45 -0500

    .

--- Processing1.py
+++ Processing1.py
@@ -4,40 +4,16 @@ Created on Jul 7, 2014
 
 @author: roj-idl71
 '''
-import os
-import sys
- 
 
 from schainpy.controller import Project
 
-from os.path import join
-
  
-
-
-path= ''
-
-local=''
- 
- 
-#minuto=2
- 
- 
-
-
-
-  
-dpath = '/home/pc-igp-173/Documentos/Cosmic Rays/data_test/'
-path_out = '/home/pc-igp-173/Documentos/Cosmic Rays/out/'
+dpath = '/home/pc-igp-173/Documentos/JRO-CosmicRays/raw_data/'
+path_out = '/home/pc-igp-173/Documentos/JRO-CosmicRays/processed_data/'
  
 if __name__ == '__main__':
-     
-     #Dia 9 6:15 a 6:50
-     #Dia 8 7:00 a 7:50
-    
-     #Grupo ofical 
-     #Dia 9 -> 6 am a 9:30 am
-        
+
+
         desc = "MLT experiment "
         filename = "schain.xml"
         startDate ='2023/06/05'
@@ -45,18 +21,8 @@ if __name__ == '__main__':
         startTime = '00:00:00'
         endTime = '23:00:00'
         dpath =dpath
-        figpath = "/media/arx/83fc24b6-cce4-46e4-a060-1525d0b03904/soporte/armando/files/saves"
-        #procpath = "/mnt/sdb1/mlt/hdf5/main"
-        remotefolrepositoryder = "/home/wmaster/graficos"
-        db_range = ['25', '60']
-        tiempo = ['7.0', '8.0']
- 
-        velocity = ['-80', '80']
-        period = 60
- 
         channels = '0,1,2'
-        profiles = '(0, 2499)'
- 
+        code = '[[1,1,1,1,1,-1,-1,1,1,-1,1,-1,1]]'
 
         controllerObj = Project()
         controllerObj.setup(id='191', name='MLT meteors', description=desc)
@@ -79,8 +45,6 @@ if __name__ == '__main__':
         opObj11 = voltage.addOperation(name='selectChannels')
         opObj11.addParameter(name='channelList', value=channels, format='int')
 
-        code = '[[1,1,1,1,1,-1,-1,1,1,-1,1,-1,1]]'
-
         opObj11 = voltage.addOperation(name='Decoder', optype='other')
         opObj11.addParameter(name='code', value=code, format='floatlist')
         opObj11.addParameter(name='nCode', value='1', format='int')
@@ -89,8 +53,8 @@ if __name__ == '__main__':
         nCohInt=2
         CohInt = voltage.addOperation(name='CohInt', optype='other')
         CohInt.addParameter(name='n', value=nCohInt, format='int')
+
     #---------------------------------------Spectra---------------------------------------
-       
         spectra = controllerObj.addProcUnit(datatype='Spectra', inputId=readUnitConfObj1.getId())
         spectra.addParameter(name='nFFTPoints', value='64')
         
@@ -101,6 +65,8 @@ if __name__ == '__main__':
         opObj12.addParameter(name='path', value=path_out)
         opObj12.addParameter(name='blocksPerFile', value='120', format='int')
                 
+
+    #---------------------------------------Start---------------------------------------
         controllerObj.start()
         
         
